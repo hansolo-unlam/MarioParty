42c851d065dd28b7e3b5e4c4478121f1a26cb9ce
LucasTeijo <54475564+LucasTeijo@users.noreply.github.com>
2019-09-21 17:58:29 -0300

    Update Jugador.java
    Hice el constructor distinto al del diagrama para que se invoque cuando se crea la partida, y reciba el usuario y el casillero de inicio de la partida

--- src/hansolo/marioparty/entidades/Jugador.java
+++ src/hansolo/marioparty/entidades/Jugador.java
@@ -1,5 +1,49 @@
 package hansolo.marioparty.entidades;
 
-public class Jugador {
+import java.util.Random;
 
+public class Jugador {
+	
+	private Usuario user;
+	private int monedas, orden, estrellas;
+	private boolean turnoJugado;
+	private Casillero posicion;
+	private Item[] items;
+	
+	public Jugador(Usuario user, Casillero start) {
+		
+		this.user = user;
+		this.monedas = this.estrellas = 0;
+		this.turnoJugado = false;
+		this.posicion = start;
+	}
+	
+	public void tirarDado() {
+		
+		int randomInt;
+		Random r = new Random();
+		if(dado normal)					//en las condiciones puse lo que deberia comprobar 
+			randomInt = r.nextInt(6-1);//dado los tipos de dados que tenemosno se como enviariamos un item o si creamos una clase dado
+		else							//no se como enviariamos un item o si creamos una clase dado
+			if(dado doble)				//segun eso habria que generar las condiciones y agregar mas si me falta algun dado
+				randomInt = r.nextInt(6-1)*2;
+			else
+				randomInt = r.nextInt(6-1)+5;
+		this.avanzar(randomInt);
+	}
+	
+	public void avanzar(int cant) {
+		
+		this.posicion = this.posicion.next;
+		for(int i=1; i<cant; i++) {
+			//aca falta chequear si es una bifurcacion y elegir un camino 
+			this.posicion = this.posicion.next;
+		}
+		this.posicion.efecto();
+	}
+	
+	public void terminarTurno() {
+		
+		this.turnoJugado = true;
+	}
 }
