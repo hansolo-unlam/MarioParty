a8a39f179d5d89f106c7c4347c7e6a31fa75dd33
Facundo Tourn <facundotourn@outlook.com>
2019-09-21 22:44:00 -0300

    Modificado el path de la prueba en el main de Launcher

--- src/hansolo/marioparty/Launcher.java
+++ src/hansolo/marioparty/Launcher.java
@@ -3,9 +3,12 @@ package hansolo.marioparty;
 import hansolo.marioparty.tablero.Tablero;
 
 public class Launcher {
-	private static final String path = "C:\\Users\\Diego\\Desktop\\Eclipse\\MarioParty\\docs\\mapa.txt";
+	private static final String path = "./recursos/mapa.txt";
 
 	public static void main(String[] args) {
+		// Linea que muestra el path de ejecución
+		// System.out.println(new File(".").getCanonicalPath());
+		
 		Tablero tb = new Tablero(path);
 		System.out.println();
 	}
