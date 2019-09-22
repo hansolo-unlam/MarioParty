f90de1a5721a71804f5a8ab502e44eafe4ea7655
LucasTeijo <54475564+LucasTeijo@users.noreply.github.com>
2019-09-22 14:33:54 -0300

    Implemente los metodos basicos en la clase Sala

--- src/hansolo/marioparty/admin/Sala.java
+++ src/hansolo/marioparty/admin/Sala.java
@@ -1,5 +1,35 @@
 package hansolo.marioparty.admin;
 
+import hansolo.marioparty.CondicionVictoria;
+import hansolo.marioparty.Partida;
+import hansolo.marioparty.tablero.Tablero;
+
 public class Sala {
+	
+	private String nombre;
+	private Usuario[] users;
+	private Partida partida;
+	private Tablero tablero;
+	private CondicionVictoria condicionV;
+	
+	public Sala (String nombre, Usuario[] users) {
+		
+		this.nombre = nombre;
+		this.users = users;
+	}
+	
+	public void comenzarPartida() {
+		
+		this.partida = new Partida(users, tablero, condicionV);
+	}
+	
+	//Deberia poder seleccionarse el tablero y la condicion desde la sala
+	public void seleccionarCondicion() {
+		
+	}
+	
+	public void seleccionarTablero() {
+		
+	}
 	
 }
