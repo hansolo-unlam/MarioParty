aabb2e3cd542d144e6bfbe9ff62f16e08a7367a9
Facundo Tourn <facundotourn@outlook.com>
2019-10-12 19:06:28 -0300

    actualizado tamaño de ventana y ubicacion  del mapa

--- src/hansolo/marioparty/Launcher.java
+++ src/hansolo/marioparty/Launcher.java
@@ -2,7 +2,7 @@ package hansolo.marioparty;
 
 public class Launcher {
 	public static void main(String[] args) {
-		Juego juego = new Juego("Mario party", 832, 896);
+		Juego juego = new Juego("Mario party", 832, 768);
 		juego.start();
 	}
 }
