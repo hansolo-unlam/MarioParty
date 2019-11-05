a3d6fd86cbcd09c57c0784f0ba9ff5ddb6fcddfc
StankoDiego <diegooxd1995@gmail.com>
2019-11-05 15:22:56 -0300

    interfaz Sala en paquete interfaz

---
+++ src/hansolo/marioparty/interfaz/Sala.java
@@ -0,0 +1,81 @@
+package hansolo.marioparty.interfaz;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Sala {
+
+	private JFrame frame;
+	private JPanel contentPane;
+	private int WIDTH = 410;
+	private int HEIGHT = 310;
+
+	private JLabel lblJugadores;
+	private JTextPane textPane;
+	private JButton btnSalirButton;
+
+	public Sala(String nombre) {
+		init(nombre);
+	}
+
+	private void init(String nombreSala) {
+		frame = new JFrame(nombreSala);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblJugadores = new JLabel("JUGADORES");
+		lblJugadores.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblJugadores.setHorizontalAlignment(SwingConstants.CENTER);
+		lblJugadores.setBounds(31, 22, 138, 32);
+		lblJugadores.setVisible(true);
+
+		contentPane.add(lblJugadores);
+
+		textPane = new JTextPane();
+		textPane.setBounds(41, 52, 138, 198);
+		textPane.setEditable(false);
+
+		contentPane.add(textPane);
+
+		btnSalirButton = new JButton("Salir de la sala");
+		btnSalirButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Tocaste salir");
+			}
+		});
+		btnSalirButton.setBounds(241, 182, 120, 23);
+		contentPane.add(btnSalirButton);
+
+		JButton btnComenzarButton = new JButton("Comenzar");
+		btnComenzarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Empezar partida");
+			}
+		});
+		btnComenzarButton.setBounds(239, 216, 122, 23);
+		contentPane.add(btnComenzarButton);
+	}
+
+	public static void main(String[] args) {
+		Sala sala = new Sala("SALA 1");
+	}
+}
