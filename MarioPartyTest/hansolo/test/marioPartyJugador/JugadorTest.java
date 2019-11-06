43eb6f4ce469f534a88e9bb1beaf6fbd3b206cac
cancellarapaula <Cancellarapaula>
2019-11-06 11:17:34 -0300

    Test jugador y dado funcionando

--- MarioPartyTest/hansolo/test/marioPartyJugador/JugadorTest.java
+++ MarioPartyTest/hansolo/test/marioPartyJugador/JugadorTest.java
@@ -8,11 +8,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 import hansolo.marioparty.CondicionVictoria;
+import hansolo.marioparty.Juego;
 import hansolo.marioparty.TipoCondicionVictoria;
 import hansolo.marioparty.admin.Usuario;
 import hansolo.marioparty.entidades.Jugador;
 import hansolo.marioparty.items.DadoDoble;
+import hansolo.marioparty.items.DadoSimple;
+import hansolo.marioparty.states.TableroState;
+import hansolo.marioparty.tablero.Casillero;
 import hansolo.marioparty.tablero.Tablero;
+import hansolo.marioparty.tablero.casilleros.MonedaCasillero;
 
 public class JugadorTest {
 	
@@ -22,7 +27,7 @@ public class JugadorTest {
 	private Jugador j1; 
 	private CondicionVictoria condicionVictoriaTest;
 	private Usuario[] usuariosTest=new Usuario[2];
-	
+	private Juego juego1;
 
 	@Before
 	public void SetUp() {
@@ -32,18 +37,18 @@ public class JugadorTest {
 		this.usuariosTest[0]=uTest1;
 		this.usuariosTest[1]=uTest2;
 		this.condicionVictoriaTest=new CondicionVictoria(TipoCondicionVictoria.ESTRELLAS,3);
-		//this.partida1= new Partida(usuariosTest,tablero,condicionVictoriaTest);
-		//this.j1= new Jugador(usuariosTest[0],tablero.getStart(),partida1);
+		this.juego1 = new Juego("Mario party", 1000, 768);
+		
+		this.j1= new Jugador(1,usuariosTest[0],juego1);
 	}
 	
 	
 	@Test
 	public void avanzar() throws Exception {
-		while (j1.getCantMovimientos() != 1)
-			j1.tirarDado();
-		
-		//j1.avanzar();
-		Assert.assertEquals(1, j1.getPosicion().getId());
+		j1.setCantMovimientos(5);
+		int resultado = DadoSimple.tirar();
+		j1.setCantMovimientos(resultado);
+		Assert.assertEquals(resultado, j1.getCantMovimientos());
 	}
 	
 	@Test
@@ -53,13 +58,14 @@ public class JugadorTest {
 	}
 	@Test
 	public void getMonedasTest() throws Exception {
-		//j1 tendria  que avanzar hasta el casillero que gana monedas j1.avanzar(1);
-		j1.setMonedas(3);
+		MonedaCasillero mc = new MonedaCasillero(1,1);
+		j1.setMonedas(mc.getMoneda());
 		Assert.assertEquals(3, j1.getMonedas());
 	}
 	
 	@Test
-	public void turnoJugadoTest() {
+	public void terminarTurno() {
+		
 		
 	}
 
--- MarioPartyTest/hansolo/test/marioparty/items/DadoDobleTest.java
+++ MarioPartyTest/hansolo/test/marioparty/items/DadoDobleTest.java
@@ -7,11 +7,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import hansolo.marioparty.CondicionVictoria;
+import hansolo.marioparty.Juego;
 //import hansolo.marioparty.Partida;
 import hansolo.marioparty.TipoCondicionVictoria;
 import hansolo.marioparty.admin.Usuario;
 import hansolo.marioparty.entidades.Jugador;
 import hansolo.marioparty.items.DadoDoble;
+import hansolo.marioparty.items.Item;
 import hansolo.marioparty.tablero.Tablero;
 
 public class DadoDobleTest {
@@ -21,6 +23,7 @@ public class DadoDobleTest {
 	private Jugador j1; 
 	private CondicionVictoria condicionVictoriaTest;
 	private Usuario[] usuariosTest=new Usuario[2];
+	private Juego juego1;
 	
 
 	@Before
@@ -31,42 +34,47 @@ public class DadoDobleTest {
 		this.usuariosTest[0]=uTest1;
 		this.usuariosTest[1]=uTest2;
 		this.condicionVictoriaTest=new CondicionVictoria(TipoCondicionVictoria.ESTRELLAS,3);
+		this.juego1 =  new Juego("Mario party", 1000, 768);;
 		//this.partida1= new Partida(usuariosTest,tablero,condicionVictoriaTest);
-		//this.j1= new Jugador(usuariosTest[0],tablero.getStart(),partida1);
+		this.j1= new Jugador(1,usuariosTest[0],juego1);
 	}
 
 	@Test
-	public void DadoDobleTest() {
-		//j1.getItem(0).setCantidad(5);
-		j1.setMonedas(8);
+	public void DadoDobleTestMonedas() {
 		
-		//j1.getItem(0).usar(j1);
+		j1.setMonedas(5);
+		DadoDoble dd = new DadoDoble(1);
+		dd.usar(j1);
+		Assert.assertEquals(2, j1.getMonedas());
 		
-		Assert.assertEquals(5, j1.getMonedas());
-		//Assert.assertEquals(4, j1.getItem(0).getCantidad());
 	}
 	
+	@Test
+	public void DadoDobleTestCantidad() {
+		
+		
+		DadoDoble dd = new DadoDoble(3);
+		dd.usar(j1);
+		Assert.assertEquals(2, dd.getCantidad());
+		
+	}
 	
 	@Test
 	public void cantidadInsuficienteDadoDobleTest() {
-		//j1.getItem(0).setCantidad(0);
-		j1.setMonedas(8);
+		DadoDoble dd = new DadoDoble(0);
+		dd.usar(j1);
+		dd.setCantidad(1);
+		Assert.assertEquals(1, dd.getCantidad());
 		
-		//j1.getItem(0).usar(j1);
-		
-		Assert.assertEquals(8, j1.getMonedas());
-		//Assert.assertEquals(0, j1.getItem(0).getCantidad());
 	}
 	
 	@Test
 	public void monedasInsuficienteDadoDobleTest() {
-		//j1.getItem(0).setCantidad(3);
-		j1.setMonedas(DadoDoble.PRECIO-1);
+		DadoDoble dd = new DadoDoble(3);
+		j1.setMonedas(2);
+		dd.usar(j1);
+		Assert.assertEquals(2, j1.getMonedas());
 		
-		//j1.getItem(0).usar(j1);
-		
-		Assert.assertEquals(DadoDoble.PRECIO-1, j1.getMonedas());
-		//Assert.assertEquals(3, j1.getItem(0).getCantidad());
 	}
 
 }
